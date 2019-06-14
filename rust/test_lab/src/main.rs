aae481d95fc6e0df9df4c357043c6a039c016596
Yon <valandil.yon@gmail.com>
2019-06-14 02:53:17 +0200

    base game plus ou moins finished gst

--- rust/test_lab/src/main.rs
+++ rust/test_lab/src/main.rs
@@ -2,10 +2,11 @@ use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::io;
 use std::io::Write;
+use std::process;
 
 //improve: systeme de record (persistence des donées en écrivant dans un fichier)
 //         système de niveaux de difficulté, la range du nombre a deviner
-//opti:    variable score, functions, immutabilité (tries)
+//opti:    variable score, functions, immutabilité (tries) / references (secret_number, et params user.0, ..)
 
 fn max_tries(tries: i32, secret_number: u32) -> i32 {
     let mut copy = tries;
@@ -13,59 +14,61 @@ fn max_tries(tries: i32, secret_number: u32) -> i32 {
         if copy <= 0 {
             println!("Maximum tries gone, you failed !
             secret number was: {}\n", secret_number);
+            process::exit(0x0100);
         }
         return copy;
 }
 
-/*
-fn get_number() -> i32 {
-    //main loop here
-}
-*/
-
-fn game() ->
-
-fn main() {
-    let _nb = 42; //le _ mute les warning de non utilisation
-    let mut tries = 10;
-    let secret_number: u32 = thread_rng().gen_range(0, 101);
+fn get_input(tries: i32, secret_number: u32) -> (i32, u32) {
+    let mut copy = tries;
     let mut input = String::new();
-
-    println!("Entrez un nombre entre 0 et 100 :");
+    let input_to_int: u32;
 
     loop {
         print!("{}: {} chance yet\n",
-            if tries > 1 { "tries"} else {"try"},
-            tries);
-        io::stdout().flush().unwrap();
+            if copy > 1 { "tries"} else {"try"},
+            copy);
 
+        io::stdout().flush().unwrap();
         input.clear();
         io::stdin()
             .read_line(&mut input)
             .expect("Error for reading line.");
 
-        //converti en nombre l'input utilisateur, et check les erreurs
-        //let input_to_int: u32 = input.trim().parse::<u32>().unwrap(); //pareil mais avec le turbofish operator
-        let input_to_int: u32 = match input.trim().parse() {
+        input_to_int = match input.trim().parse::<u32>() {
             Ok(t) => t,
             Err(_) => {
                 println!("Not a valid number !\n");
-                tries = max_tries(tries, secret_number);
+                copy = max_tries(copy, secret_number);
                 continue;
-            }
+            },
         };
+        break;
+    }
+    return (copy, input_to_int);
+}
 
-        //pattern matching avec cmp
-        match input_to_int.cmp(&secret_number) {
+fn main() {
+    let _nb = 42; //just a memo: le _ mute les warning de non utilisation
+    let tries = 7;
+    let input_to_int: u32 = 0;
+    let secret_number: u32 = thread_rng().gen_range(0, 101);
+    let mut user = (tries, input_to_int);
+
+    println!("Entrez un nombre entre 0 et 100 :");
+
+    loop {
+        user = get_input(user.0, secret_number);
+
+        // Pattern matching avec cmp
+        match user.1.cmp(&secret_number) {
             Ordering::Less => println!("Number is bigger!"),
             Ordering::Greater => println!("Number is smaller!"),
             Ordering::Equal => {
-                println!("You win!\nYour score: {}", 11 - tries);
+                println!("You win!\nYour score: {}", 11 - user.0);
                 break;
             }
         }
-
-        //check nombre d'essais max
-        tries = max_tries(tries, secret_number);
+        user.0 = max_tries(user.0, secret_number);
     }
 }
